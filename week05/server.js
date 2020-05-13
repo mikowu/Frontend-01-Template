22b18c25313c5754862b82e0d75b2c1022184f2f
mikowu <37141891+mikowu@users.noreply.github.com>
2020-05-13 23:11:54 +0800

    Create server.js

---
+++ week05/server.js
@@ -0,0 +1,10 @@
+
+const http=require("http");
+const server = http.createServer((req, res) => {
+    res.setHeader('X-Foo', 'bar');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('ok');
+    console.log(res)
+  });
+server.listen(8088);
+
